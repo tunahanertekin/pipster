ffa637a6289e9b79aa6e801993212f9e6d191f07
tunahan ertekin <tunahan.ertekin13@gmail.com>
2022-03-03 13:47:31 +0300

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+export TERM=xterm
 red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 blue=`tput setaf 4`
 magenta=`tput setaf 5`
 reset=`tput sgr0`
-export TERM=xterm
 while [ true ]
 do 
     echo "${blue}waiting for command...${reset}"; 
