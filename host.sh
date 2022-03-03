ef1fd721833075ed3a162e87cb5e36d15e560da5
tunahan ertekin <tunahan.ertekin13@gmail.com>
2022-03-03 12:04:52 +0300

    Create host.sh

---
+++ host.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
+while [ true ]
+do 
+    echo "${blue}waiting for command...${reset}"; 
+    rcv=$(cat $PIPES/api/request); 
+    printf '%s %s\n' "$(date): ${yellow}[ $rcv ]${reset} received."
+    eval $rcv > $PIPES/api/response; 
+    printf '%s %s\n' "$(date): ${blue}output sent${reset}"
+done
